3c6894eb0ae424fb72866b056ff0f2df0e35b1db
GuilhermeRaphael <guigaorapha07@gmail.com>
2025-03-14 22:36:10 -0300

    Aula 06 TipCalculator

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnNormalTip(object sender, EventArgs e)
+        {
+            double valor = Convert.ToDouble(billInput.Text);
+            double percentualDaGorjeta = tipPercentSlider.Value;
+
+            double gorjeta = valor * (percentualDaGorjeta / 100);
+            double total = valor + gorjeta;
+
+            //Manipular para aparecer em um elemento que ja existe
+            tipOutput.Text = gorjeta.ToString(); // Convert.ToString(gorjeta)
+            totalOutput.Text = total.ToString(); // Convert.ToString(total)
+        }
+
+        private void OnGenerousTip(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+
+}
